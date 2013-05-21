ad56a862e34ecdc1bc807758dde14c53ffd3a713
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-05-20 22:32:25 -0600

    Updated scaffolding for creating components

--- commands/command-create-component.js
+++ commands/command-create-component.js
@@ -1,19 +1,70 @@
 var File = require('raptor/files/File'),
     files = require('raptor/files'),
-    path = require('path');
+    path = require('path'),
+    raptor = require('raptor');
+
+
+var gradients = [
+    ["hsl(220,97%,90%)", "hsl(220,97%,70%)"],
+    ["hsl(346,96%,90%)", "hsl(346,96%,70%)"],
+    ["hsl(107,87%,90%)", "hsl(107,87%,70%)"],
+    ["hsl(53,87%,90%)", "hsl(53,87%,70%)"],
+    ["hsl(299,82%,90%)", "hsl(299,82%,70%)"],
+    ["hsl(27,100%,90%)", "hsl(27,100%,70%)"],
+    ["hsl(318,100%,90%)", "hsl(318,100%,70%)"],
+    ["hsl(74,75%,90%)", "hsl(74,75%,70%)"],
+    ["hsl(17,47%,90%)", "hsl(17,47%,70%)"],
+    ["hsl(309,85%,90%)", "hsl(309,85%,70%)"],
+    ["hsl(91,78%,90%)", "hsl(91,78%,70%)"],
+    ["hsl(6,100%,90%)", "hsl(6,100%,70%)"],
+    ["hsl(70,38%,90%)", "hsl(70,38%,70%)"]
+];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomGradient() {
+  var index = getRandomInt(0, gradients.length-1);
+  return gradients[index];
+}
 
 function getComponentInfo(name) {
     var lastSlash = name.lastIndexOf('/'),
         shortName = lastSlash === -1 ? name : name.slice(lastSlash+1),
-        shortNameLower = shortName.toLowerCase(),
         shortNameDashSeparated = shortName.replace(/([a-z])([A-Z])/g, function(match, a, b) {
             return a + '-' + b;
         }).toLowerCase();
+
+    var isLower = shortName.charAt(0) === shortName.charAt(0).toLowerCase();
+    var widgetName;
+    var rendererName;
+    var widgetFilename;
+    var rendererFilename;
+
+    if (isLower) {
+        widgetName = name + '/' + shortName + '-widget';
+        widgetFilename = shortName + '-widget';
+        rendererName = name + '/' + shortName + '-renderer';
+        rendererFilename = shortName + '-renderer';
+    }
+    else {
+        widgetName = name + '/' + shortName + 'Widget';
+        widgetFilename = shortName + 'Widget';
+        rendererName = name + '/' + shortName + 'Renderer';
+        rendererFilename = shortName + 'Renderer';
+    }
+
     return {
         name: name,
         shortName: shortName,
-        shortNameLower: shortNameLower,
-        shortNameDashSeparated: shortNameDashSeparated
+        widgetName: widgetName,
+        widgetFilename: widgetFilename,
+        rendererFilename: rendererFilename,
+        rendererName: rendererName,
+        tagName: shortNameDashSeparated,
+        cssClassName: shortNameDashSeparated,
+        templateName: name
     };
 }
 
@@ -24,6 +75,10 @@ module.exports = {
         'no-widget': {
             boolean: true,
             describe: 'Do not generate a widget'
+        },
+        'overwrite': {
+            'boolean': true,
+            describe: 'Overwrite existing files'
         }
     },
 
@@ -35,19 +90,33 @@ module.exports = {
 
         return {
             name: name,
-            includeWidget: args['widget'] !== false
+            includeWidget: args['widget'] !== false,
+            overwrite: args['overwrite'] === true
         };
     },
 
     run: function(args, config, rapido) {
         var name = args.name;
-        var baseDir = config['components.dir'] || process.cwd();
-        var outputDir = new File(baseDir, name);
-        var namespace = config['component.namespace'] || config['module.namespace'];
-        if (namespace) {
-            name = namespace + '/' + name;
+
+        if (name.startsWith('/')) {
+            name = name.substring(1);
         }
 
+        if (name.endsWith('/')) {
+            name = name.slice(0, -1);
+        }
+
+        var baseDir = config['components.dir'] || process.cwd();
+        
+        var prefix = config['components.prefix'];
+        if (prefix) {
+            if (prefix.endsWith('/')) {
+                prefix = prefix.slice(0, -1);
+            }
+            name = prefix + '/' + name;
+        }
+
+        var outputDir = new File(baseDir, name);
         var componentInfo = getComponentInfo(name);
         var scaffoldDir = config['scaffold.component.dir'];
 
@@ -55,17 +124,22 @@ module.exports = {
             throw new Error('"scaffold.component.dir" not defined in "' + rapido.configFilename + '" config file');
         }
 
+        var gradient = getRandomGradient();
+
+        var data = {
+            ifWidget: args.includeWidget,
+            gradientStart: gradient[0],
+            gradientEnd: gradient[1],
+        };
+
+        raptor.extend(data, componentInfo);
+
         rapido.scaffold(
             {
                 scaffoldDir: scaffoldDir,
                 outputDir: outputDir,
-                data: {
-                    ifWidget: args.includeWidget,
-                    name: componentInfo.name,
-                    shortName: componentInfo.shortName,
-                    shortNameLower: componentInfo.shortNameLower,
-                    shortNameDashSeparated: componentInfo.shortNameDashSeparated
-                },
+                overwrite: args.overwrite,
+                data: data,
                 afterFile: function(outputFile) {
                     // Register RTLD files in the app.rtld file
                     if (outputFile.getExtension() === 'rtld') {
@@ -93,7 +167,7 @@ module.exports = {
         rapido.log('Or, to render client-side, add the following instead:');
         rapido.log.info('<module name="' + name + '/render"/>');
         rapido.log('\nCustom tag usage:');
-        rapido.log.info('<app:' + componentInfo.shortNameDashSeparated + ' name="Frank" count="30"/>');
+        rapido.log.info('<app:' + componentInfo.tagName + ' name="Frank" count="30"/>');
     },
 
     getComponentInfo: getComponentInfo
